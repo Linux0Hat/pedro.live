b7f8b4d6df922b5321631d98266fc67f7eef52ce
Linux_Hat <adrien@d4x.fr>
2024-04-28 20:22:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# pedro.live
+
+Based on [`parrot.live`](https://github.com/hugomd/parrot.live), `pedro.live` allow any computer with `curl` to display an _animated_ dancing racoon named pedro.
+
+# Try it!
+
+```bash
+curl pedro.linuxhat.net
+```
+
+# :partypedro:
+
+![plot](./images/5.png)
+
+# Thanks
+
+- [hugomd](https://github.com/hugomd) for [`parrot.live`](https://github.com/hugomd/parrot.live)
+- fleksa30 on tiktok for the dancing racoon video.
+
+# More Annimations
+
+- [cultofthepartyparrot.com](http://cultofthepartyparrot.com/)
+- [`parrot.live`](https://github.com/hugomd/parrot.live)
+- [`terminal-parrot`](https://github.com/jmhobbs/terminal-parrot)
+- [`parrotsay`](https://github.com/matheuss/parrotsay)
+- [`ascii.live`](https://github.com/hugomd/ascii.live)
---
+++ index.js
@@ -0,0 +1,80 @@
+const fs = require("mz/fs");
+const path = require("path");
+const http = require("http");
+const url = require("url");
+const { Readable } = require("stream");
+
+// Path to the directory containing the .txt files
+const directory = "frames";
+
+let frames = [];
+
+// Utiliser async/await pour charger les frames de manière synchrone
+async function loadFrames() {
+  try {
+    const files = await fs.readdir(directory);
+
+    // Trier les noms de fichiers par ordre alphabétique
+    files.sort((a, b) => {
+      const numA = parseInt(a.split(".")[0]); // Extraire le numéro du nom de fichier
+      const numB = parseInt(b.split(".")[0]);
+      return numA - numB;
+    });
+
+    for (const file of files) {
+      if (file.endsWith(".txt")) {
+        const filePath = path.join(directory, file);
+        const content = await fs.readFile(filePath, "utf8");
+        frames.push(content);
+      }
+    }
+  } catch (err) {
+    console.error("Error loading frames:", err);
+  }
+}
+
+// Appeler la fonction pour charger les frames
+loadFrames();
+
+const streamer = (stream) => {
+  let index = 0;
+
+  return setInterval(() => {
+    // Effacer l'écran
+    stream.push("\033[2J\033[3J\033[H");
+    stream.push(frames[index]);
+    index = (index + 1) % frames.length;
+  }, 200);
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/healthcheck") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify({ status: "ok" }));
+  }
+
+  if (
+    req.headers &&
+    req.headers["user-agent"] &&
+    !req.headers["user-agent"].includes("curl")
+  ) {
+    res.writeHead(302, { Location: "https://github.com/Linux0Hat/pedro.live" });
+    return res.end();
+  }
+
+  const stream = new Readable();
+  stream._read = function noop() {};
+  stream.pipe(res);
+  const interval = streamer(stream);
+
+  req.on("close", () => {
+    stream.destroy();
+    clearInterval(interval);
+  });
+});
+
+const port = process.env.PARROT_PORT || 3000;
+server.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`Listening on localhost:${port}`);
+});
